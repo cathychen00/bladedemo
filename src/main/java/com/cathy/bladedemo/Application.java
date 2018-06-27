c48c5d907987d86f0a3c3e1a0c95df3c4ec3d8b1
chenjing <chenjing@autohome.com.cn>
2018-06-27 16:18:27 +0800

    Initial commit

---
+++ src/main/java/com/cathy/bladedemo/Application.java
@@ -0,0 +1,11 @@
+package com.cathy.bladedemo;
+
+import com.blade.Blade;
+
+public class Application {
+    public static void main(String[] args) {
+        Blade.me().get("/", (request, response) -> {
+            response.text("Hello World");
+        }).start();
+    }
+}
